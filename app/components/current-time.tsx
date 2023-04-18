13cbf993dc9bbf4c35732c174859241ec2b3b507
agadzik <andrew.gadzik@vercel.com>
2023-04-18 10:27:43 -0400

    use dynamic function

--- app/components/current-time.tsx
+++ app/components/current-time.tsx
@@ -1,8 +1,10 @@
+import { headers } from "next/headers";
+
 export async function CurrentTime(): Promise<JSX.Element> {
   const res = await fetch("https://httpbin.org/anything");
   const data = await res.json();
 
-  console.log(res.ok, data.url);
+  console.log(res.ok, headers());
 
   return <div>Current time: {new Date().toLocaleTimeString()}</div>;
 }
